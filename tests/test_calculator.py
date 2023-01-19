6e4d9678a5e1eed0d2d8b01a9954775658c857d8
HenriM <henrimaronen@gmail.com>
2023-01-19 14:30:13 +0200

    basic test templates done

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,39 +1,40 @@
 import pytest
 import requests
 from fastapi.testclient import TestClient
-from ..main import app
-
 
 import json
 
 FEE_URL = "http://localhost:8000/delivery_fee"
 ROOT_URL = "http://localhost:8000"
 
-client = TestClient(app)
-
-
-def test_read_root():
-	response = client.get("/")
+def test_landing_page_status():
+	response = requests.get(ROOT_URL)
 	assert response.status_code == 200
-	assert response.json() == {"root": "root"}
-
-# def test_landing_page_status():
-# 	response = requests.get(ROOT_URL)
-# 	assert response.status_code == 200
 	
-# def test_landing_page_content():
-# 	expected = {"root":"root"}
-# 	response = requests.get(ROOT_URL)
-# 	assert response.json() == expected
+def test_landing_page_content():
+	expected = {"root":"root"}
+	response = requests.get(ROOT_URL)
+	assert response.json() == expected
 
-# def test_delivery_fee_endpoint():
-# 	payload = {
-# 		"cart_value": 0,
-# 		"delivery_distance": 1500,
-# 		"number_of_items": 0,
-# 		"time": "string"
+def test_delivery_fee_status():
+	cart = {
+		"cart_value": 0,
+		"delivery_distance": 1500,
+		"number_of_items": 0,
+		"time": "string"
 
-# 	}
+	}
+	response = requests.get(FEE_URL, json = cart)
+	assert response.status_code == 200
 
-# 	response = requests.post(FEE_URL, payload)
-# 	assert response.status_code == 200
+def test_delivery_fee_return_value():
+
+	cart = {
+		"cart_value": 890,
+		"delivery_distance": 1500,
+		"number_of_items": 0,
+		"time": "string"
+	}
+	response = requests.get(FEE_URL, json = cart)
+	data = json.loads(response.content)
+	assert data == {'delivery_fee': 300}
